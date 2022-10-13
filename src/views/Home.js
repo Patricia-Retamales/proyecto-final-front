b5ce0dd5b572fa5692c763d46199cb3ae1046cc1
angelicamaliqueo <angelicamaliqueor@gmail.com>
2022-10-13 00:20:47 +0000

    subo las cards

--- src/views/Home.js
+++ src/views/Home.js
@@ -2,12 +2,15 @@ import React, { useEffect, useContext } from 'react';
 import { Context } from "../store/AppContext";
 import ComponentNavbar from "../componet/ComponentNavbar";
 import Productos from "../componet/Productos"
+import ComponentCarrusel from "../componet/ComponentCarrusel";
 
 const Home = () =>{
     return(
 <>
 <ComponentNavbar/>
+<ComponentCarrusel/>
 <Productos />
+
 </>
     );
 }

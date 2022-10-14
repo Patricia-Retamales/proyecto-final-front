084986c1658529d95156b480e521179bee13c90e
joce2311 <jnortegamontecino@gmail.com>
2022-10-14 01:18:18 +0000

    footer actualizado..

--- src/componet/ComponenteFooter.js
+++ src/componet/ComponenteFooter.js
@@ -7,14 +7,48 @@ import {FaInstagram } from "react-icons/fa";
 export const Footer = () => (
 	<footer className="footer mt-auto py-3 text-center">
 		<p>
-        <h6> Facebook < FaFacebookSquare /></h6>
-        <h6> Instagram  < FaInstagram /></h6>
-        <h6> Github < FaGithubSquare /></h6>
+    
+		<dif class="row p-5 bg-dark text-white">  
 
-			Made with <i className="" /> by{" "}
-			<a href="">De todo un poquito!!</a>
+		  <dif class="col-xs-12 col-md-6 col-lg-3">
+			<p class="h3"> Contenido </p>
+			<div class="mb-2">
+				<a class="text-white text-decoration-none" href="#">De todo un poquito va dirigido a los pequeños emprendedores, en el cual estan iniciando con grandes ideas y sueños . </a>
+				</div>
+		     </dif>
 
+		  <dif class="col-xs-12 col-md-6 col-lg-3">
+			<p class="h5"> Terminos y Condiciones  </p>
+			<div class="mb-2">
+				<a class="text-white text-decoration-none" href="#">Politica de privacidad 
+				</a>
+			</div>
+			
+			<a class="text-white text-decoration-none" href="#"> 100% Confiable </a>
+		  </dif>
+
+		  <dif class="col-xs-12 col-md-6 col-lg-3">
+			<p class="h5 mb-3"> Contacto </p>
+			<div class="mb-2">
+				<a class="text-white text-decoration-none" href> <h6> Instagram  < FaInstagram /></h6></a>
+			</div>
+            <div class="mb-2">
+				<a class="text-white text-decoration-none" href> <h6> Github < FaGithubSquare /></h6></a>
+			</div>
+            <div class="mb-2">
+				<a class="text-white text-decoration-none" href> <h6> Facebook < FaFacebookSquare /></h6></a>
+			</div>
             
+		  </dif>
+ 
+		
+		 <dif class="col-xs-12 pt-5">
+			<p class="text-white text-center">Colaboradores Santiago ,Angelica, Patricia y joceline .2022</p>
+		 </dif>
+       
+         </dif> 
+
+		
 		</p>
 	</footer>
 );

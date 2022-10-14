e4086f1eeaeb91c1ee54150464df108a00299450
sneelyg <santiagoneely@gmail.com>
2022-10-14 22:33:55 +0000

    login funcionando con back-end

--- src/componet/ComponentNavbar.js
+++ src/componet/ComponentNavbar.js
@@ -15,70 +15,70 @@ function ComponentNabar() {
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="#features"><Link to="productos">Productos</Link></Nav.Link>
-            
-            <NavDropdown  title="Producto" id="collasible-nav-dropdown">
+
+            <NavDropdown title="Producto" id="collasible-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">
-               <Link to="/marcas"> Semillas</Link>
-                </NavDropdown.Item>
+                <Link to="/marcas"> Semillas</Link>
+              </NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
                 <Link to="/cupcakeparadise">Tortas</Link>
-                </NavDropdown.Item>
+              </NavDropdown.Item>
               <NavDropdown.Item href="#action/3.3">
-              <Link to="/disfracesfantasia">Disfrases</Link>
-                </NavDropdown.Item>
+                <Link to="/disfracesfantasia">Disfraces</Link>
+              </NavDropdown.Item>
               <NavDropdown.Item href="#action/3.4">
-              <Link to="/elestudiante">Utiles Escolares</Link> 
-                </NavDropdown.Item>
+                <Link to="/elestudiante">Utiles Escolares</Link>
+              </NavDropdown.Item>
               <NavDropdown.Item href="#action/3.5">
-              <Link to='bellapandora'>joyas</Link> 
-                </NavDropdown.Item>
+                <Link to='bellapandora'>joyas</Link>
+              </NavDropdown.Item>
               <NavDropdown.Item href="#action/3.6">
-              <Link to="sabordivino">Chocolate</Link>
-                </NavDropdown.Item>
+                <Link to="sabordivino">Chocolate</Link>
+              </NavDropdown.Item>
               <NavDropdown.Item href="#action/3.7">
-              <Link to="elquijotedelamancha">limpieza</Link> 
-                </NavDropdown.Item>
-            
+                <Link to="elquijotedelamancha">limpieza</Link>
+              </NavDropdown.Item>
+
               <NavDropdown.Item href="#action/3.8">
-               <Link to="/latiendita">Ropa</Link>
+                <Link to="/latiendita">Ropa</Link>
               </NavDropdown.Item>
             </NavDropdown>
           </Nav>
 
           <Link to="/login">
-				<button className="btn btn-primary">Ingresar</button>
-				</Link>
-        <Link to="/registration">
-				<button className="btn btn-primary">Registrar</button>
-				</Link>
+            <button className="btn btn-primary">Ingresar</button>
+          </Link>
+          <Link to="/registration">
+            <button className="btn btn-primary">Registrar</button>
+          </Link>
 
 
 
-          <Nav ClassName="me-auto">
+          <Nav className="me-auto">
             <NavDropdown title="Usuario" id="collasible-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">
                 <Link to="/datosusuario">configuracion</Link>
-                </NavDropdown.Item>
-                
+              </NavDropdown.Item>
+
               <NavDropdown.Item href="#action/3.2">
-               Vender
+                Vender
               </NavDropdown.Item>
               <NavDropdown.Item href="#action/3.3">
                 Comprar
-                </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-              Mis favoritos
               </NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
-               Lista
+                Mis favoritos
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Lista
               </NavDropdown.Item>
               <NavDropdown.Divider />
               <NavDropdown.Item href="#action/3.4">
                 Cerrar sesion
               </NavDropdown.Item>
             </NavDropdown>
-            </Nav>
-            
+          </Nav>
+
         </Navbar.Collapse>
       </Container>
     </Navbar>
@@ -86,4 +86,4 @@ function ComponentNabar() {
 }
 
 export default ComponentNabar;
-  ;
+;
--- src/componet/Login.js
+++ src/componet/Login.js
@@ -1,37 +1,58 @@
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import ComponentNavbar from './ComponentNavbar';
-import React, { useContext } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import Footer from "../componet/ComponenteFooter";
-
+import { Context } from "../store/AppContext.js"
 
 function Login() {
+  const { store, actions } = useContext(Context);
+  const [login_email, setEmail] = useState("");
+  const [login_password, setPassword] = useState("");
 
-
+  let datos_login = {};
   return (
 
     <Form>
       <ComponentNavbar />
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
-      </Form.Group>
+      <div>
+        <form justify="center" style={{ width: "38rem" }}>
+          <div className="mb-3">
+            <label className="form-label">Email address</label>
+            <input type="email" className="form-control"
+              onChange={e => setEmail(e.target.value)}
+              value={login_email}
+              id="exampleInputEmail1" aria-describedby="emailHelp"
+              placeholder="email@e-mail.com" />
+            <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+          </div>
+          <div className="mb-3">
+            <label className="form-label">Password</label>
+            <input type="password" className="form-control"
+              onChange={e => setPassword(e.target.value)}
+              value={login_password}
+              id="exampleInputPassword1" placeholder="password" />
+          </div>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-      <Footer/>
+          <button type="submit" className="btn btn-primary"
+            onClick={(e) => {
+              e.preventDefault();
+              {
+                datos_login = {
+                  "email": login_email,
+                  "password": login_password
+                }
+              };
+              actions.login_function(datos_login);
+            }} >
+            Submit
+          </button>
+        </form>
+      </div>
+
+      <Footer />
     </Form>
+
   );
 }
 

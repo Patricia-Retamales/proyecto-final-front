8de78bfb8fd98fa1dfea6ee2b0f65fb4521236c1
sneelyg <santiagoneely@gmail.com>
2022-10-15 00:05:34 +0000

    arreglando formulario de registro

--- src/componet/Registration.js
+++ src/componet/Registration.js
@@ -4,83 +4,104 @@ import Row from 'react-bootstrap/Row';
 import Button from 'react-bootstrap/Button';
 import { Link } from 'react-router-dom';
 import ComponentNabar from './ComponentNavbar';
-
-
+import { Context } from "../store/AppContext.js"
+import React, { useEffect, useState, useContext } from "react";
 
 function Registration() {
+  const { store, actions } = useContext(Context);
+  const [signup_email, setEmail] = useState("");
+  const [signup_password, setPassword] = useState("");
+  const [signup_username, setUsername] = useState("");
+  const [apelldo, setApellidod] = useState("");
+  const [nombre, setNombre] = useState("");
+  const [rut, setRut] = useState("");
+
+
+  let datos_registro = {
+    "email": "",
+    "password": "",
+    "is_active": "True",
+    "username": "",
+    "nombre": "",
+    "apellido": "",
+    "rut": 1
+  }
+
   return (
     <Form>
       <ComponentNabar />
-      <Row>
 
-        <Col>
-          <Form.Control placeholder=" name" />
-        </Col>
-        <Col>
-          <Form.Control placeholder="surname" />
-        </Col>
-      </Row>
-      <Form.Group className="mb-3" controlId="formGridAddress2">
-        <Form.Label>Rut</Form.Label>
-        <Form.Control placeholder="enter the route" />
-      </Form.Group>
-      <Row className="mb-3">
-        <Form.Group as={Col} controlId="formGridEmail">
+      <div className="row mx-5">
+
+        <div className="col-md-6">
+          <p>Nombre</p>
+          <Form.Control placeholder=" Nombre" />
+        </div>
+
+        <div className="col-md-6">
+          <p>Apellido</p>
+          <Form.Control placeholder="Apellido"  />
+        </div>
+
+        <div className=" col-md-6" controlId="formGridAddress2">
+          <Form.Label>Rut</Form.Label>
+          <Form.Control placeholder="1234567-8" />
+        </div>
+
+        <div className="col-md-6" controlId="formGridEmail">
           <Form.Label>Email</Form.Label>
           <Form.Control type="email" placeholder="Enter email" />
-        </Form.Group>
-        <Form.Group as={Col} controlId="formGridEmail">
+        </div>
+        <div className="col-md-6" controlId="formGridEmail">
           <Form.Label>RepeatEmail</Form.Label>
           <Form.Control type="email" placeholder="Enter email" />
-        </Form.Group>
-        <Form.Group as={Col} controlId="formGridPassword">
+        </div>
+        <div className="col-md-6" controlId="formGridPassword">
           <Form.Label>Password</Form.Label>
           <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
-        <Form.Group as={Col} controlId="formGridPassword">
+        </div>
+        <div className="col-md-6" controlId="formGridPassword">
           <Form.Label>RepeatPassword</Form.Label>
           <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
-      </Row>
+        </div>
 
-      <Form.Group className="mb-3" controlId="formGridAddress1">
-        <Form.Label>direccion</Form.Label>
-        <Form.Control placeholder="1234 Main St" />
-      </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formGridAddress2">
-        <Form.Label>casa o depto</Form.Label>
-        <Form.Control placeholder="casa o depto" />
-      </Form.Group>
+        <div className="col-md-6" controlId="formGridAddress1">
+          <Form.Label>direccion</Form.Label>
+          <Form.Control placeholder="1234 Main St" />
+        </div>
 
-      <Row className="mb-3">
-        <Form.Group as={Col} controlId="formGridCity">
+        <div className="col-md-6" controlId="formGridAddress2">
+          <Form.Label>casa o depto</Form.Label>
+          <Form.Control placeholder="casa o depto" />
+        </div>
+
+
+        <div className="col-md-6" controlId="formGridCity">
           <Form.Label>City</Form.Label>
           <Form.Control />
-        </Form.Group>
+        </div>
 
-        <Form.Group as={Col} controlId="formGridState">
+        <div className="col-md-6" controlId="formGridState">
           <Form.Label>State</Form.Label>
           <Form.Select defaultValue="Choose...">
             <option>Choose...</option>
             <option>Region Metropolitana</option>
           </Form.Select>
-        </Form.Group>
+        </div>
 
-        <Form.Group as={Col} controlId="formGridZip">
+        <div className="col-md-6" controlId="formGridZip">
           <Form.Label>Zip</Form.Label>
           <Form.Control />
-        </Form.Group>
-      </Row>
+        </div>
+        <Link to="/Home">
+          <Button variant="primary" type="submit" >
+            Submit
+          </Button>
+        </Link>
+      </div>
 
-      <Form.Group className="mb-3" id="formGridCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
-      <Link to="/Home">
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </Link>
+
     </Form>
 
   );

29505db3ac6dc4cbbca97e8adc34a98b580bb11a
patricia <retamalespatricia01@gmail.com>
2022-10-25 00:02:18 +0000

    contraseña

--- src/componet/Recuperar_clave.js
+++ src/componet/Recuperar_clave.js
@@ -15,73 +15,29 @@ function Recuperar_Clave() {
 
       <ComponentNavbar />
 
-      <Form justify="center" style={{ width: "38rem" }}>
-
-        <div className="mb-3">
-          <label className="form-label">Ingresa tu correo</label>
-          <input type="email" className="form-control"
-            onChange={e => setEmail(e.target.value)}
-            value={login_email}
-            id="exampleInputEmail1" aria-describedby="emailHelp"
-            placeholder="email@e-mail.com" />
-          <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
-        </div>
-
-        <Link to="/">
-          <button type="submit" className="btn btn-primary"
-            onClick={(e) => {
-              e.preventDefault();
-              actions.recuperar_clave_funcion(login_email);
-
-
-            }} >
-            Submit
-          </button>
-        </Link>
-
-        
-        <Form.Label htmlFor="inputPassword5">Ingresar pin</Form.Label>
-        <div style={{ width: "40%", display: "flex", justifyContent: "space-between" , }}>
-          <Form.Control style={{right:"20px"}}
-            type="password"
-            id="inputPassword5"
-            aria-describedby="passwordHelpBlock"
-          />
-         
-          <Form.Control
-            type="password"
-            id="inputPassword5"
-            aria-describedby="passwordHelpBlock"
-          />
-          <Form.Control
-            type="password"
-            id="inputPassword5"
-            aria-describedby="passwordHelpBlock"
-          />
-          <Form.Control
-            type="password"
-            id="inputPassword5"
-            aria-describedby="passwordHelpBlock"
-          />
-        </div>
-
-     
-      </Form>
-
 
       <div>
         <form justify="center" style={{ width: "38rem" }}>
-          
+
           <div className="mb-3">
-            <label className="form-label">Ingresa tu correo</label>
-            <input type="email" className="form-control"
-              onChange={e => setEmail(e.target.value)}
-              value={login_email}
-              id="exampleInputEmail1" aria-describedby="emailHelp"
-              placeholder="email@e-mail.com" />
-            <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+          <InputGroup className="mb-3">
+        <InputGroup.Text id="basic-addon1">@</InputGroup.Text>
+        <Form.Control
+          placeholder="Username"
+          aria-label="Username"
+          aria-describedby="basic-addon1"
+        />
+      </InputGroup>
+              // onChange={e => setEmail(e.target.value)}
+              // value={login_email}
+              
           </div>
 
+          <div>
+            <label htmlFor="phone">celular</label>
+            <input id="phone" type="tel" placeholder="+56" />
+          </div>
+          <br/>
           <Link to="/cambiar_clave">
             <button type="submit" className="btn btn-primary"
               onClick={(e) => {
@@ -97,7 +53,7 @@ function Recuperar_Clave() {
       </div>
 
 
-        <Footer />
+      <Footer />
     </form >
 
   );

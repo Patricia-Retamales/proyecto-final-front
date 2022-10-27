6809d03887ffebbf8d373e5b789fe1f2cd62fd1f
joce2311 <jnortegamontecino@gmail.com>
2022-10-27 02:04:01 +0000

    centradodefooter

--- src/componet/ComponenteFooter.js
+++ src/componet/ComponenteFooter.js
@@ -6,13 +6,13 @@ import { FaInstagram } from "react-icons/fa";
 
 function Footer() {
 	return (
-		<footer className="footer mt-auto py-3 text-center"  >
+		<footer className="footer mt-auto py-3 text-center"  style={{ displey :"flex", justifyContent: "flex-end"}}>
 
 
 
 			<div className="row p-5 bg- text-white" style={{ background: "#888a8a", border: "##9b9b9b 5px solid" , }}>
 
-				<div className="col-xs-12 col-md-6 col-lg-3">
+				<div className=" col-lg-4">
 					<p className="h3"> Contenido  </p>
 					<div className="mb-2">
 						<a className="text-white text-decoration-none" href="#">De todo un poquito va dirigido a los
@@ -20,7 +20,7 @@ function Footer() {
 					</div>
 				</div>
 
-				<div className="col-xs-12 col-md-6 col-lg-3">
+				<div className=" col-lg-4">
 					<p className="h5"> Terminos y Condiciones  </p>
 					<div className="mb-2">
 						<a className="text-white text-decoration-none" href="#">Politica de privacidad
@@ -30,7 +30,7 @@ function Footer() {
 					<a className="text-white text-decoration-none" href="#"> 100% Confiable </a>
 				</div>
 
-				<div className="col-xs-12 col-md-6 col-lg-3">
+				<div className=" col-lg-4">
 					<p className="h5 mb-3"> Contacto </p>
 					<div className="mb-2">
 						<a className="text-white text-decoration-none" href> <h6> Instagram  < FaInstagram /></h6></a>
@@ -39,14 +39,14 @@ function Footer() {
 						<a className="text-white text-decoration-none" href> <h6> Github < FaGithubSquare /></h6></a>
 					</div>
 					<div className="mb-2">
-						<a className="text-white text-decoration-none" href> <h6> Facebook < FaFacebookSquare /></h6></a>
+						<a className="text-white text-decoration-none-right" href> <h6> Facebook < FaFacebookSquare /></h6></a>
 					</div>
 
 				</div>
 
 
 				<div className="col-xs-12 pt-5">
-					<p className="text-white text-center">Colaboradores Santiago, Angelica, Patricia y joceline * 2022</p>
+					<p className="text-white text-center">Colaboradores Santiago, Angelica, Patricia y Joceline * 2022</p>
 				</div>
 
 			</div>
